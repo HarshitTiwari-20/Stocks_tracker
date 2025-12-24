161d6c89b510f15a00dcacf19d647c2db36af400
HarshitTiwari-20 <ht337378@gmail.com>
2025-12-24 15:47:29 +0530

    stock market tracker-6

--- components/TradingWidget.tsx
+++ components/TradingWidget.tsx
@@ -1,25 +1,32 @@
 'use client';
 
-// TradingViewWidget.jsx
-import React, { useEffect, useRef, memo } from 'react';
 
-function TradingWidget() {
-  const container = useRef(null);
+import React, { useRef, memo } from 'react';
 
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = ``;
-      container.current.appendChild(script);
-    },
-    []
-  );
+
+interface TradingWidgetProps {
+  title?: string;
+  scriptUrl?: string;
+  config?: Record<string, unknown>;
+  height?: number;
+  className?: string;
+}
+
+
+
+
+
+function TradingWidget({
+  title, scriptUrl, config, height = 600, className
+}: TradingWidgetProps)
+
+{
+  const containerRef = useRef(null);
+
+
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
+    <div className="tradingview-widget-container" ref={containerRef} style={{ height: "100%", width: "100%" }}>
       <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
       <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/XAUUSD/?exchange=OANDA" rel="noopener nofollow" target="_blank"><span className="blue-text">XAUUSD chart</span></a><span className="trademark"> by TradingView</span></div>
     </div>
