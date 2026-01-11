e10c9a1b9b59f15ad70298d7ce76de9f1bb83652
HarshitTiwari-20 <ht337378@gmail.com>
2026-01-11 22:33:48 +0530

    error handling

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,7 @@ const Header = async ({ user }: { user?: User }) => {
       <header className=" sticky  top-0 header">
         <div className=" container header-wrapper">
           <Link href="/">
-            <Image src="/assets/images/logo.png" alt="Logo" width={140} height={40} className=" h-8 w-auto cursor-pointer" />
+            <Image src="/assets/icons/logo.svg" alt="Logo" width={140} height={40} className=" h-8 w-auto cursor-pointer" />
           </Link>
           <nav className="hidden sm:block">
             <NavItems initialStocks={initialStocks}/>
